e662776fc0458b37d99eba6abed0fbdcf30ef7a0
Robert <robert.hamilton281@gmail.com>
2022-08-18 12:55:15 -0500

    Fixed unit tests & started building out logic

--- RHDC.LeetCode/RHDC.LeetCode.Tests/15.3Sum.cs
+++ RHDC.LeetCode/RHDC.LeetCode.Tests/15.3Sum.cs
@@ -1,8 +1,6 @@
-using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Reflection.PortableExecutable;
+using System.Linq;
 
 namespace RHDC.LeetCode.Tests
 {
@@ -12,33 +10,44 @@ namespace RHDC.LeetCode.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var numbers = new int[]{-1,0,1,2,-1,-4};
+            var numbers = new int[] { -1, 0, 1, 2, -1, -4 };
             var result = FindThreeSums(numbers);
-            var expected = new int[][] { new int[]{ -1, -1, 2 }, new int[] { -1, 0, 1 } };
+            var expected = new int[][] { new int[] { -1, -1, 2 }, new int[] { -1, 0, 1 } };
             Assert.AreEqual(expected, result);
         }
 
-        private int[][] FindThreeSums(int[] numbers)
+        private IEnumerable<int[]> FindThreeSums(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
-        }
+            var answers = new List<int[]>();
+            if (numbers.Count() < 3)
+            {
+                return answers;
+            }
+            numbers = numbers.Select(x => x).Distinct();
+            var targetResult = 0;
+            foreach (var number in numbers)
+            {
+            }
 
-        [TestMethod]
-        public void TestMethod3()
-        {
-            var numbers = new int[] { 0};
-            var result = FindThreeSums(numbers);
-            var expected = new int[][] { };
-            Assert.AreEqual(expected, result);
+            return answers;
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            var numbers = new int[] {};
+            var numbers = new int[] { 0, 1, 1 };
             var result = FindThreeSums(numbers);
-            var expected = new int[][] {};
-            Assert.AreEqual(expected, result);
+            var expected = new int[][] { };
+            Assert.IsTrue(Enumerable.SequenceEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var numbers = new int[] { 0, 0, 0 };
+            var result = FindThreeSums(numbers).ToArray();
+            var expected = new int[][] { };
+            Assert.IsTrue(Enumerable.SequenceEqual(expected, result));
         }
     }
 }
